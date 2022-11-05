39ed456d70ef1778fa70d6ff867928d8ba7a0353
Hau519 <vuhau19@gmail.com>
2022-11-05 11:56:10 -0400

    change teacher file

--- app_config.py
+++ app_config.py
@@ -1,4 +1,4 @@
-CONST_MONGO_URL = "mongodb+srv://pythonls3:112358@cluster0.1j5tkho.mongodb.net/?retryWrites=true&w=majority"
+CONST_MONGO_URL = "mongodb+srv://pythonls3:vMdBTmZFeylsNJKX@cluster0.7s4my0s.mongodb.net/?retryWrites=true&w=majority"
 CONST_DATABASE = "LS3"
 CONST_USER_COLLECTION = "users"
 
