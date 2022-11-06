90bdbc8c61d01978a2ec2322cb71188efa187d56
Gabriel Valente <98720580+gabvalente@users.noreply.github.com>
2022-11-06 14:53:13 -0500

    Update user_view.py
    Added comments to help with documentation looking further.

--- views/user_view.py
+++ views/user_view.py
@@ -1,3 +1,8 @@
+# this view is the endpoint accessed by the user.
+# this file will serve as a "sub-route".
+# the API acts as a "waiter" delivering requests from the front to the backend.
+# "JSON Web Token is a proposed Internet standard for creating data with optional signature and/or optional encryption whose payload holds JSON that asserts some number of claims."
+
 from flask import Blueprint, request, jsonify
 from database.__init__ import database
 from models.user_model import User
@@ -5,7 +10,7 @@ import json
 from controllers.user_controller import createUser, loginUser, fetchUsers
 from helpers.token_validation import validateJWT
 
-user = Blueprint("user", __name__)
+user = Blueprint("user", __name__)  # for backwards compatibility, the framework requires a name + the file name.
 
 @user.route("/v0/users/signup", methods=["POST"])
 def create():
@@ -28,6 +33,7 @@ def create():
             return jsonify({'error': 'Something wrong happened when creating user.'}), 500
 
         return jsonify({'uid': str(createdUser.inserted_id)})
+
     except ValueError:
         return jsonify({'error': 'Error creating user.'}), 400
 
