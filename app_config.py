1b88e0eac71e7c46817a9093f9761f8f2d759b0d
Gabriel Valente <98720580+gabvalente@users.noreply.github.com>
2022-11-06 19:31:49 -0500

    Added comments to help with documentation.

--- app_config.py
+++ app_config.py
@@ -1,6 +1,8 @@
-CONST_MONGO_URL = "mongodb+srv://pythonls3:vMdBTmZFeylsNJKX@cluster0.7s4my0s.mongodb.net/?retryWrites=true&w=majority"
-CONST_DATABASE = "LS3"
-CONST_USER_COLLECTION = "users"
+# saving constant information to be used across program.
+
+CONST_MONGO_URL = "mongodb+srv://pythonls3:vMdBTmZFeylsNJKX@cluster0.7s4my0s.mongodb.net/?retryWrites=true&w=majority"  # the url used to connect to the db.
+CONST_DATABASE = "LS3"  # the database name.
+CONST_USER_COLLECTION = "users"  # one of the collections. Collections in mongodb = tables in SQL.
 
 TOKEN_SECRET = "iosandpython"
 JWT_EXPIRATION = 86400*30
