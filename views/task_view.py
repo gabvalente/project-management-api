c3aef1efd4f69b56adc5c338a7d4f3560e04e84f
Marietagu <112421879+Marietagu@users.noreply.github.com>
2022-11-12 10:19:21 -0500

    edit valid_object_id

--- views/task_view.py
+++ views/task_view.py
@@ -3,8 +3,19 @@ from controllers.task_controller import create_task, fetch_created_task, fetch_a
     checking_task_in_list
 import json
 from helpers.token_validation import validateJWT
+from bson.errors import InvalidId
+from bson.objectid import ObjectId
 
 task = Blueprint("task", __name__)
+
+
+def is_valid(oid):
+
+    try:
+        ObjectId(oid)
+        return True
+    except (InvalidId, TypeError):
+        return False
 
 
 @task.route("/v0/tasks/createTask", methods=["POST"])
@@ -12,9 +23,9 @@ def create():
     try:
         token = validateJWT()
         if token == 400:
-            return jsonify({'error': 'Token is missing in the request.'}), 400
+            return jsonify({'error': 'Token is missing in the request.'}), 401
         if token == 401:
-            return jsonify({'error': 'Invalid authentication token.'}), 401
+            return jsonify({'error': 'Invalid authentication token.'}), 403
 
         data = json.loads(request.data)
 
@@ -22,6 +33,9 @@ def create():
             return jsonify({'error': 'Description is needed in the request.'}), 400
         if 'assignedToUid' not in data:
             return jsonify({'error': 'Assigned user is needed in the request.'}), 400
+
+        if is_valid(data['assignedToUid']) is False:
+            return jsonify({'error': 'Uid is not valid.'}), 400
 
         created_task = create_task(token, data)
         return jsonify({'uid': str(created_task.inserted_id)})
@@ -35,9 +49,9 @@ def created_by():
     try:
         token = validateJWT()
         if token == 400:
-            return jsonify({'error': 'Token is missing in the request.'}), 400
+            return jsonify({'error': 'Token is missing in the request.'}), 401
         if token == 401:
-            return jsonify({'error': 'Invalid authentication token.'}), 401
+            return jsonify({'error': 'Invalid authentication token.'}), 403
 
         return fetch_created_task(token['id'])
 
@@ -50,9 +64,9 @@ def assigned_to():
     try:
         token = validateJWT()
         if token == 400:
-            return jsonify({'error': 'Token is missing in the request.'}), 400
+            return jsonify({'error': 'Token is missing in the request.'}), 401
         if token == 401:
-            return jsonify({'error': 'Invalid authentication token.'}), 401
+            return jsonify({'error': 'Invalid authentication token.'}), 403
 
         return fetch_assigned_task(token['id'])
     except ValueError:
@@ -64,9 +78,9 @@ def update_task(task_uid):
     try:
         token = validateJWT()
         if token == 400:
-            return jsonify({'error': 'Token is missing in the request.'}), 400
+            return jsonify({'error': 'Token is missing in the request.'}), 401
         if token == 401:
-            return jsonify({'error': 'Invalid authentication token.'}), 401
+            return jsonify({'error': 'Invalid authentication token.'}), 403
         data = json.loads(request.data)
         if 'done' not in data:
             return jsonify({'error': 'Done is needed in the request.'}), 400
@@ -86,9 +100,9 @@ def delete_task(task_uid):
     try:
         token = validateJWT()
         if token == 400:
-            return jsonify({'error': 'Token is missing in the request.'}), 400
+            return jsonify({'error': 'Token is missing in the request.'}), 401
         if token == 401:
-            return jsonify({'error': 'Invalid authentication token.'}), 401
+            return jsonify({'error': 'Invalid authentication token.'}), 403
 
         list_task = fetch_created_task(token['id'])
         task_check = checking_task_in_list(list_task, task_uid)
