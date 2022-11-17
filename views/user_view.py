2097bf3cd55f851c9a91cf879f83bcdd6d967ee1
Gabriel Valente <98720580+gabvalente@users.noreply.github.com>
2022-11-17 17:49:58 -0500

    Code cleanup.

--- views/user_view.py
+++ views/user_view.py
@@ -2,15 +2,11 @@
 # this file will serve as a "sub-route".
 # the API acts as a "waiter" delivering requests from the front to the backend.
 # "JSON Web Token is a proposed Internet standard for creating data with optional signature and/or optional encryption whose payload holds JSON that asserts some number of claims."
-import email
-
-from flask import Blueprint, request, jsonify
-from database.__init__ import database
-from models.user_model import User
 import json
+from flask import Blueprint, request, jsonify
 from controllers.user_controller import createUser, loginUser, fetchUsers
-from helpers.token_validation import validateJWT
 from helpers.email_validation import validate_email
+from helpers.token_validation import validateJWT
 
 user = Blueprint("user", __name__)  # for backwards compatibility, the framework requires a name + the file name.
 
